1b8478522f9765489d7a0188d7e80ccdbc01bc8c
gerardodev25 <gerardodev25@gmail.com>
2021-06-25 21:30:34 -0400

    add navBar

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('heelo world');
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+
+hamburger.addEventListener("click", () => {
+   nav.classList.add("nav-show");
+   nav.classList.remove("nav-hidden");
+});
+
+nav.addEventListener("click", (e) => {
+   console.log(e);
+   if (
+      e.target.classList.contains("nav__closet") ||
+      e.target.classList.contains("nav__link")
+   ) {
+      nav.classList.remove("nav-show");
+      nav.classList.add("nav-hidden");
+   }
+});
